7e1b03901dc14a4c4da4d08ce2d5a5d48bfbf6ff
Grimmys <funkyzermmy@gmail.com>
2021-07-03 15:56:25 +0200

    Release 1.1.0
    Changed the way of providing backgroud images to components

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pygame-popup",
-    version="0.1.0",
+    version="0.1.1",
     author="Grimmys",
     author_email="grimmys.programming@gmail.com",
     description="A popup manager for pygame",

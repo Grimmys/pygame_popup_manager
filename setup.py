74fd25952a1180a5ee8c710d1f7fc64b5b7eaad4
Grimmys <funkyzermmy@gmail.com>
2021-09-10 21:49:40 +0200

    Release 0.3.2
    * Change name of "set_info_box_title" method in configuration to "set_info_box_title_font"
    * Added a default size for dynamic buttons

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pygame-popup",
-    version="0.3.1",
+    version="0.3.2",
     author="Grimmys",
     author_email="grimmys.programming@gmail.com",
     description="A popup manager for pygame",

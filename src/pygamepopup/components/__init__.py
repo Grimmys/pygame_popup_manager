97c56f446f7d0c1c6c7518a6b29f903554ecf9ca
grimmys <funkyzermmy@gmail.com>
2026-08-08 12:51:19 +0200

    Add init import for Image component
    Covers #9

--- src/pygamepopup/components/__init__.py
+++ src/pygamepopup/components/__init__.py
@@ -1,6 +1,7 @@
 from .box_element import BoxElement
 from .button import Button
 from .dynamic_button import DynamicButton
+from .image import Image
 from .image_button import ImageButton
 from .info_box import InfoBox
 from .text_element import TextElement
